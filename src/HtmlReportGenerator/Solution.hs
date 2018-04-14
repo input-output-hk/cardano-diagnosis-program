bf5736043c3158c546f0450f75b48fd6eb390daa
HirotoShioi <hiroto.shioi@iohk.io>
2018-04-14 17:18:30 +0900

    Highlight certain keyword in the text to make them readable

--- src/HtmlReportGenerator/Solution.hs
+++ src/HtmlReportGenerator/Solution.hs
@@ -6,6 +6,7 @@ module HtmlReportGenerator.Solution
        ) where
 
 import qualified Data.Text.Lazy              as LT
+import Data.Semigroup ((<>))
 import           Text.Blaze.Html5
 import qualified Text.Blaze.Html5.Attributes as A
 
@@ -23,46 +24,49 @@ renderSolution _             t = renderSimpleSolution t
 renderSimpleSolution :: LT.Text -> Html
 renderSimpleSolution str = p ! A.class_ "card-text" $ toHtml str
 
+bold :: LT.Text -> Html
+bold str = p ! A.class_ "font-weight-bold" $ toHtml str
+
 -- | Render solution for DBError
 renderDBError :: LT.Text -> Html
 renderDBError str =
   p ! A.class_ "card-text" $ do
     p $ toHtml str
-    p ! A.class_ "font-weight-bold" $ "Win"
+    bold "Win"
     ol $ do
-      li "Type %appdata% on the explorer’s address bar"
-      li "Open Daedalus folder"
-      li "Delete the content of DB-1.0"
-    p ! A.class_ "font-weight-bold" $ "Mac"
+      li $ "Type " <> mark "%appdata%" <> "on the explorer’s address bar"
+      li $ "Open " <> mark "Daedalus" <> " folder"
+      li $ "Delete the content of " <> mark "DB-1.0"
+    bold "Mac"
     ol $ do
-      li "On the Finder’s menu bar select 'Go'"
-      li "Click 'Go to Finder…'"
-      li "Type '~/Library/Application Support/Daedalus'"
-      li "Delete the content of 'DB-1.0'"
+      li $ "On the Finder’s menu bar select " <> mark "Go"
+      li $ "Click " <> mark "Go to Finder…"
+      li $ "Type " <> mark "~/Library/Application Support/Daedalus"
+      li $ "Delete the content of " <> mark "DB-1.0"
 
 -- | Render solution for Time sync
 renderTimeSync :: LT.Text -> Html
 renderTimeSync str =
   p ! A.class_ "card-text" $ do
     p $ toHtml str
-    p ! A.class_ "font-weight-bold" $ "Win"
+    bold "Win"
     ol $ do
-      li "Press Win+S"
-      li "Type in Settings"
-      li "In 'Find a setting field' type in 'Date'"
-      li "Go to 'Date and time settings'"
-      li "Press 'Additional date, time & regional settings'. New window will pop up"
-      li "Press 'Set date and time'"
-      li "Go to 'Internet time tab'"
-      li "Press 'Change settings'"
-      li "Press Update now Note: there should be a message like The clock was successfully synchronized"
-    p ! A.class_ "font-weight-bold" $ "Mac"
+      li $ "Press " <> mark "Win+S"
+      li $ "Type in " <> mark "Settings"
+      li $ "In " <> mark "Find a setting field" <> " type in " <> mark "Date"
+      li $ "Go to " <> mark "Date and time settings"
+      li $ "Press " <> mark "Additional date, time & regional settings" <> ". New window will pop up"
+      li $ "Press " <> mark "Set date and time"
+      li $ "Go to " <> mark "Internet time tab"
+      li $ "Press " <> mark "Change settings'"
+      li $ "Press " <> mark "Update now" <> "Note: there should be a message like The clock was successfully synchronized"
+    bold "Mac"
     ol $ do
       li "Quit the application"
       li "Click the clock that is located on the top right of the screen"
-      li "Select 'Open Date & Time Preferences'"
+      li $ "Select " <> mark "Open Date & Time Preferences'"
       li "Unlock the preferences"
-      li "Uncheck the 'Set date and time automatically'"
+      li $ "Uncheck the " <> mark "Set date and time automatically"
       li "Restart the application"
 
 -- | Render solution for stale lock file
@@ -70,17 +74,17 @@ renderStaleLockFile :: LT.Text -> Html
 renderStaleLockFile str =
   p ! A.class_ "card-text" $ do
     p $ toHtml str
-    p ! A.class_ "font-weight-bold" $ "Win"
+    bold "Win"
     ol $ do
       li "Quit the application"
-      li "Type %appdata% on the explorer’s search bar"
-      li "Open Daedalus folder"
-      li "Open 'Wallet-1.0' and delete file 'open.lock'"
+      li $ "Type " <> mark "%appdata%" <> "on the explorer’s search bar"
+      li $ "Open " <> mark "Daedalus" <> " folder"
+      li $ "Open " <> mark "Wallet-1.0" <> " and delete file " <> mark "open.lock"
       li "Start the application"
-    p ! A.class_"font-weight-bold" $ "Mac"
+    bold "Mac"
     ol $ do
-      li "On the Finder’s menu bar select 'Go'"
-      li "Click 'Go to Finder…'"
-      li "Type '~/Library/Application Support/Daedalus'"
-      li "Open 'Wallet-1.0' and delete file 'open.lock'"
+      li $ "On the Finder’s menu bar select " <> mark "Go"
+      li $ "Click " <> mark "Go to Finder…"
+      li $ "Type " <> mark "~/Library/Application Support/Daedalus"
+      li $ "Open " <> mark "Wallet-1.0" <> "and delete file " <> mark "open.lock"
       li "Start the application"
