124b7bf993d2003d973947b580ee27ddb3f449a0
HirotoShioi <hiroto.shioi@iohk.io>
2018-04-20 20:17:41 +0900

    Use foldl' instead of foldr to improve performance

--- src/Classifier.hs
+++ src/Classifier.hs
@@ -6,6 +6,7 @@ module Classifier
        ) where
 
 import qualified Data.ByteString.Lazy     as LBS
+import           Data.List                (foldl')
 import qualified Data.Map.Strict          as Map
 import           Data.Text.Encoding.Error (ignore)
 import qualified Data.Text.Lazy           as LT
@@ -15,17 +16,17 @@ import           Types                    (Analysis, Knowledge (..))
 
 -- | Analyze each log file based on the knowlodgebases' data.
 extractIssuesFromLogs :: [LBS.ByteString] -> Analysis -> Analysis
-extractIssuesFromLogs files analysis = filterAnalysis $ foldr runClassifiers analysis files
+extractIssuesFromLogs files analysis = filterAnalysis $ foldl' runClassifiers analysis files
 
 -- | Run analysis on given file
-runClassifiers :: LBS.ByteString -> Analysis -> Analysis
-runClassifiers logfile analysis =
+runClassifiers :: Analysis -> LBS.ByteString -> Analysis
+runClassifiers analysis logfile =
     let logLines = LT.lines $ LT.decodeUtf8With ignore logfile
-    in foldr analyzeLine analysis logLines
+    in foldl' analyzeLine analysis logLines
 
 -- | Analyze each line
-analyzeLine :: LT.Text -> Analysis -> Analysis
-analyzeLine str = Map.mapWithKey (compareWithKnowledge str)
+analyzeLine :: Analysis -> LT.Text -> Analysis
+analyzeLine analysis str = Map.mapWithKey (compareWithKnowledge str) analysis
 
 -- | Compare the line with knowledge lists
 compareWithKnowledge :: LT.Text -> Knowledge -> [LT.Text] -> [LT.Text]
@@ -34,6 +35,6 @@ compareWithKnowledge str Knowledge{..} xs =
     then str : xs
     else xs
 
--- | Filter out any record that has empty (i.e couldn't catch any string related)
+-- | Filter out any records that are empty (i.e couldn't catch any string related)
 filterAnalysis :: Analysis -> Analysis
 filterAnalysis = Map.filter (/= [])
